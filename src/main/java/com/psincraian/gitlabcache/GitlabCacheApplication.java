d0d1d9816666f96dae5fa442f524912aa8a220f2
Petru Rares Sincraian <psincraian@outlook.com>
2019-12-01 21:07:00 +0100

    feat: first commit

---
+++ src/main/java/com/psincraian/gitlabcache/GitlabCacheApplication.java
@@ -0,0 +1,13 @@
+package com.psincraian.gitlabcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabCacheApplication.class, args);
+	}
+
+}
